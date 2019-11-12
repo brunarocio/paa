18610ba765edebe7921d971f8f0a1a0612b323d0
Delio H <deliodh@gmail.com>
2019-11-12 15:14:42 -0300

    verifica primalidade

--- Delio-Python/main.py
+++ Delio-Python/main.py
@@ -3,11 +3,16 @@ from random import *
 def main():
     i = 0
     j = 0
+    teste = 2**32
+    #print('2 na 128 ',teste)
     seed()
-    p = randint(10000000000000000000000000000000000,100000000000000000000000000000000000000)
-    q = randint(10000000000000000000000000000000000,100000000000000000000000000000000000000)
-    #p = randint(1000,2000)
-    #q = randint(1000,2000)
+    #2**128p = randint(1000000000000000000000000000000000000000,10000000000000000000000000000000000000000)
+    #2**32p = randint(10000000000,100000000000)
+    p = randint(1000,2000)
+    q = randint(1000,2000)
+    #p = randint(10000000000,100000000000)
+    #q = randint(10000000000,100000000000)
+    
     print('Numero gerado P: ',p,'\nQ: ',q,'\n')
     if ((p%2) == 0):
         p = p+1
@@ -29,7 +34,8 @@ def verificaPrimalidade(num):
     b = num-1
     c = (a**b)
     print('C: ',c)
-    primo = pow(a,b,num)
+    primo = c%num
+    #primo = pow(b,a,num)
     print('Primo: ',primo)
     if (primo != 1):
         return 0
