729f3d7e4155d96211e25a0138ee01f2e0f5eb5c
Delio H <deliodh@gmail.com>
2019-12-01 16:18:21 -0300

    .

--- PCV-Tabu/main_tabu.py
+++ PCV-Tabu/main_tabu.py
@@ -11,16 +11,19 @@ matrizDistancias = [[0,40,55,190,130],
 [120,135,140,312,0]]
 #Qtde de cidades
 cidades = len(matrizDistancias)
+maxIteracoes = cidades/2
+maxVizinhos = cidades/2
+melhorSolucao = []
+
 
 def main():
     #1-criar solucao inicial
     #testes distancias
     #print('Distancias ',matrizDistancias[0][1])
-    solucao = definirSolucao0(5)
-    print('Solucao 0: ',solucao)
-    distanciaSolucao = calcularFuncaoObjetiva(solucao)
+    melhorSolucao = definirSolucao0(5)
+    print('Solucao 0: ',melhorSolucao)
+    distanciaSolucao = calcularFuncaoObjetiva(melhorSolucao)
     print('Para esta solução, o tamanho da rota é ',distanciaSolucao)
-
     return 1
 
 def definirSolucao0(qtde):
@@ -50,5 +53,22 @@ def obterDistancia(origem,destino):
     origem-=1
     destino-=1
     return matrizDistancias[origem][destino]
+
+def gerarVizinhanca(melhorSolucao,maxVizinhos):
+    vizinhos = []
+    pos = 0
+    for i in range(0, maxVizinhos):
+        vizinho = []
+        for j in range(0, len(melhorSolucao)):
+            if  j == pos:
+                if melhorSolucao[j] == 0:
+                    vizinho.append(1)
+                else:
+                    vizinho.append(0)
+            else:
+                vizinho.append(melhorSolucao[j])
+        vizinhos.append(vizinho)
+        pos += 1
+        return vizinhos
 
 main()
